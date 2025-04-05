65f8c47335f492a38d59c8e339a66ded622eb677
Jayden Tan <jaytan3966@gmail.com>
2025-04-05 14:31:38 -0700

    merged connection and server files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri = process.env.ATLAS_URI || "";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,3 +24,17 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("QuizMe").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("QuizMe");
+
+export default db;
