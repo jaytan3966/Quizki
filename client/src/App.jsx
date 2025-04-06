da46779c835545e16ed15c1f0d6042f983ff2d60
EmanuelNader <EmanuelSNader@gmail.com>
2025-04-05 17:55:24 -0700

    Flashcards are working, now need to implement create flashcardlist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import FlashcardList from './components/FlashcardList.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
 
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <FlashcardList flashcards={flashcards} />
   )
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+  },
+  {
+    id: 2,
+    question: 'Question 2',
+    answer: 'Answer',
+  }
+];
+
 
 export default App
