0472ab1bd95296450388785d0e64ec1e732ce9cb
Jayden Tan <jaytan3966@gmail.com>
2025-04-05 18:26:34 -0700

    should be good to merge with jord backend

--- server/routing/records.js
+++ server/routing/records.js
@@ -11,24 +11,24 @@ router.get("/:desired", async (req, res) => {
   res.send(results).status(200);
 });
 //get specific user info
-router.get("/players/:name", async (req, res) => {
-    let collection = await db.collection("players");
+router.get("/users/:name", async (req, res) => {
+    let collection = await db.collection("users");
     let name = req.params.name;
     let results = await collection.find({name: `${name}`}).toArray();
     res.send(results).status(200);
 })
 
 //adds a new term
-router.patch("/vocab/:player/:term/:ans", async (req, res) => {
+router.patch("/vocab/:user/:term/:ans", async (req, res) => {
   try {
     let term = req.params.term;
     let answer = req.params.ans;
 
-    let user = req.params.player;
+    let user = req.params.user;
     let query = {$push: {terms : {[term]: answer}}};
 
-    let collection = await db.collection("players");
-    let result = await collection.updateOne({name: user}, query);
+    let collection = await db.collection("users");
+    let result = await collection.updateOne({email: user}, query);
     res.send(result).status(204);
   } catch (err) {
     console.error(err);
@@ -36,26 +36,25 @@ router.patch("/vocab/:player/:term/:ans", async (req, res) => {
   }
 });
 //increments points
-router.patch("/balance/:player/:amnt", async (req, res) => {
+router.patch("/balance/:user/:amnt", async (req, res) => {
     try {
-      let user = req.params.player;
+      let user = req.params.user;
       let amnt = parseInt(req.params.amnt);
       
       let query = {$inc: {points : amnt}};
   
-      let collection = await db.collection("players");
-      let result = await collection.updateOne({name: user}, query);
+      let collection = await db.collection("users");
+      let result = await collection.updateOne({email: user}, query);
       res.send(result).status(204);
     } catch (err) {
       console.error(err);
       res.status(500).send("Error adding record");
     }
   });
-
 //adds collected smiski
-router.patch("/smiskis/:player/:smiski", async (req, res) => {
+router.patch("/smiskis/:user/:smiski", async (req, res) => {
     try {
-      let user = req.params.player;
+      let user = req.params.user;
       let smiskiName = req.params.smiski;
       let smiskiCollection = await db.collection("smiskis");
       let smiskiInfo = await smiskiCollection.findOne({name: smiskiName});
@@ -63,28 +62,10 @@ router.patch("/smiskis/:player/:smiski", async (req, res) => {
       let query1 = {$push: {collected : smiskiInfo}};
       let query2 = {$inc: {points : -100}};
 
-      let collection = await db.collection("players");
-      let result1 = await collection.updateOne({name: user}, query1);
-      let result2 = await collection.updateOne({name: user}, query2);
+      let collection = await db.collection("users");
+      let result1 = await collection.updateOne({email: user}, query1);
+      let result2 = await collection.updateOne({email: user}, query2);
       res.send(result1).status(204);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Error adding record");
-    }
-  });
-
-//create new user
-router.post("/players/:name", async (req, res) => {
-    try {
-      let newDocument = {
-        name: req.params.name,
-        points: 0,
-        collected: [],
-        terms: []
-      };
-      let collection = await db.collection("players");
-      let result = await collection.insertOne(newDocument);
-      res.send(result).status(204);
     } catch (err) {
       console.error(err);
       res.status(500).send("Error adding record");
@@ -109,16 +90,16 @@ router.post("/smiskis/:name/:series/:img", async (req, res) => {
   });
 
 //resets progress
-router.delete("/:player", async (req, res) => {
-  let user = req.params.player;
-  let collection = db.collection("players");
+router.delete("/:user", async (req, res) => {
+  let user = req.params.user;
+  let collection = db.collection("users");
   try {
     let resetPoints = {$set: {points: 0}}
     let resetCollected = {$set: {collected: []}}
     let resetTerms = {$set: {terms: []}}
-    let result = await collection.updateOne({name: user}, resetPoints);
-    let resultt = await collection.updateOne({name: user}, resetCollected);
-    let resulttt = await collection.updateOne({name: user}, resetTerms);
+    let result = await collection.updateOne({email: user}, resetPoints);
+    let resultt = await collection.updateOne({email: user}, resetCollected);
+    let resulttt = await collection.updateOne({email: user}, resetTerms);
 
     res.send(result).status(200);
   } catch (err) {
