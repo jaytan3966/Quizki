fa9809acff8a2a2cedcb3f41be0c6cf4cb9fa46f
Amy Trinh <amytrinh@Amys-MacBook-Air-9.local>
2025-04-06 03:59:01 -0700

    minor changes

--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
+const LoginButton = (props) => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Login</button>;
+  return <button className={props.additionalStyles} onClick={() => loginWithRedirect()}>Login</button>;
 };
 
 export default LoginButton;
--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LogoutButton = () => {
+const LogoutButton = (props) => {
   const { logout } = useAuth0();
 
   return (
-    <button
+    <button className={props.additionalStyles}
       onClick={() =>
         logout({ logoutParams: { returnTo: window.location.origin } })
       }
     >
-      Logout
+      Log Out
     </button>
   );
 };
